6358a5666257be5fcfb802bca35f9091d50969cd
zoranm5 <zoran.musulin@gmail.com>
2024-09-22 18:18:01 +0200

    Dodani hoover efekti

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log('JavaScript radi');
+
 document.addEventListener('DOMContentLoaded', function() {
     const images = document.querySelectorAll('.gallery img');
     images.forEach(img => {
@@ -8,3 +8,18 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Spriječava ponovno učitavanje stranice
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    console.log('Ime:', name);
+    console.log('Email:', email);
+    console.log('Poruka:', message);
+
+    // Ovdje bi se mogla dodati logika za slanje podataka na server
+    alert('Hvala što ste nas kontaktirali, ' + name + '!');
+});
+
